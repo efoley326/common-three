b4f4aa03d566f5b837383609006eafb0b754fb41
Emily <efoley326@gmail.com>
2023-02-07 13:28:52 -0500

    punct and contraction tests added

--- spec/common-three_spec.rb
+++ spec/common-three_spec.rb
@@ -1,5 +1,11 @@
 require_relative "./spec_helper.rb"
 
+# for quick reference, the contents of the text .txt file:
+# oops I can't believe I messed this up. 
+# oops I can't BelievE you messed this up!
+# Oops, I messed this up. 
+# oops i cAn't believe I messed this up.  Oops you messed ThIs up
+
 RSpec.describe "Cli" do
 ARGF = File.open("./texts/test_text.txt")
 
@@ -10,5 +16,13 @@ ARGF = File.open("./texts/test_text.txt")
 
   it "can ignore casing" do
     expect(run_example.first) == ("messed this up - 5")
+  end
+  
+  it "can ignore punctuation" do
+    expect(run_example.first) == ("messed this up - 5")
+  end
+  
+  it "can take contractions as a single word" do
+    expect(run_example[2]) == ("i cant believe - 3")
   end
 end
